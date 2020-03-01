fa12168b5d4f051347e38da0dd725b959c3ec3a2
Anne de Joly <anne.dejoly@edhec.com>
2020-03-01 21:53:28 +0000

    Setup of the app and cloudinary installed

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
 
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.12.0'
+gem 'faker'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 # Use postgresql as the database for Active Record
@@ -21,6 +25,10 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.12.0'
+gem 'simple_form'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'faker'
+
+puts "Creating the ingredients..."
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+ingredients = user['drinks']
+
+doses = %w[2cl 3cl 5cl 10cl 2ml 5ml 10ml]
+
+ingredients.each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
+
+puts "Ingredients created!"
+
+puts "Mixing some cocktails..."
+
+4.times do
+  cocktail = Cocktail.create!(name: Faker::Beer.name)
+  3.times do
+    dose = Dose.create!(description: doses.sample,
+                        ingredient: Ingredient.all.sample,
+                        cocktail: cocktail)
+  end
+end
+
+puts "Cocktails served!"
